a4fb7c9f7a15d51540a43f0a53eecf10e6a816a4
elisha-enu <elishaenu@gmail.com>
2019-12-03 23:50:20 +0700

    move slider, random target

--- Bullseye/Bullseye/ContentView.swift
+++ Bullseye/Bullseye/ContentView.swift
@@ -10,26 +10,69 @@ import SwiftUI
 
 struct ContentView: View {
     @State var isAlertVisible: Bool = false
+    @State var sliderValue: Double = 50.0
+    @State var randomNumber: Int = Int.random(in: 1...100 )
     
     var body: some View {
         VStack {
-            Text("Welcome to my first app ")
-                .fontWeight(.semibold)
-                .foregroundColor(.red)
-            Button(action: {
-                print("Button pressed!")
-                self.isAlertVisible = true
-            }) {
-                Text(/*@START_MENU_TOKEN@*/"Hit me"/*@END_MENU_TOKEN@*/)
+            Spacer()
+            
+            // Target row
+            HStack{
+                Text("Put the bullseye as close as you can to: ")
+                Text("\(self.randomNumber)")
             }
-            .alert(isPresented: $isAlertVisible) { () ->
-                Alert in
-                return Alert(
-                    title: Text("Hello There!"),
-                    message: Text("This s my first pop up"),
-                    dismissButton: .default(Text("Awesome!"))
-                )
+            Spacer()
+            
+            // Slider row
+            HStack{
+                Text("1")
+                Slider(value: self.$sliderValue , in: 1...100)
+                Text("100")
             }
+            Spacer()
+            
+            // Button row
+            HStack {
+                Button(action: {
+                    print("Button pressed!")
+                    self.isAlertVisible = true
+                }) {
+                    Text(/*@START_MENU_TOKEN@*/"Hit me"/*@END_MENU_TOKEN@*/)
+                }
+                .alert(isPresented: $isAlertVisible) { () ->
+                    Alert in
+                    let roundedValue: Int = Int(self.sliderValue.rounded())
+                    return Alert(
+                        title: Text("Hello There!"),
+                        message: Text("The slider value is \(roundedValue)"),
+                        dismissButton: .default(Text("Awesome!"))
+                    )
+                }
+            }
+            Spacer()
+            
+            // Score row
+            HStack {
+                Button(action: {
+                    print("Button pressed!")
+                }) {
+                    Text("Start over")
+                }
+                Spacer()
+                Text("Score:")
+                Text("999")
+                Spacer()
+                Text("Round:")
+                Text("999")
+                Spacer()
+                Button(action: {
+                    print("Info!")
+                }) {
+                    Text("Info")
+                }
+            }
+            .padding(.bottom, 20)
         }
     }
 }
